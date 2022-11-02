677eab24dba66297efd830f024600f17f77e644a
Simone <106522803+simoluca@users.noreply.github.com>
2022-11-02 11:28:30 +0100

    1

--- IlMioCorpo/Assets/Scripts/Rotate.cs
+++ IlMioCorpo/Assets/Scripts/Rotate.cs
@@ -4,11 +4,46 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    //this allows only one coroutine at a time to run
+    private bool coroutineAllowed;
 
-    // Update is called once per frame
+    void Start()
+    {
+        // set to true to coroutine can be started initially
+        coroutineAllowed = true;
+    }
+
+    // method is invoked when mouse pointer is above object.
+    // the collider helps to detect when this event occurs
+    // when pointer is above the object and coroutine is allowed, start the animation
+    private void OnMouseOver() {
+
+        if (coroutineAllowed)
+        {
+            StartCoroutine("StartAnimation");
+        }
+        
+    }
+
+    private IEnumerator StartAnimation()
+    {
+        // initially set to false to a new coroutine will not start
+        coroutineAllowed = false;
+
+        for (float i = 0f; i <= 1f; i += 0.1f)
+        {
+            transform.Rotate(new Vector3(0f, 200f, 0f) * Time.deltaTime);   
+            yield return new WaitForSeconds(0.015f);
+        }
+         coroutineAllowed = true;
+
+    }
+
+    /*
     void Update()
     {
         //Time.deltaTime to make sure that the animation runs at the same speed on every device
         transform.Rotate(new Vector3(0f, 200f, 0f) * Time.deltaTime);   
     }
+    */
 }
