b8c26fdf71fbc489e22c894e3dbbebc64b865b6e
Simone <106522803+simoluca@users.noreply.github.com>
2022-11-10 11:54:34 +0100

    1

--- IlMioCorpo/Assets/Scripts/DragAndDrop2.cs
+++ IlMioCorpo/Assets/Scripts/DragAndDrop2.cs
@@ -9,6 +9,7 @@ public class DragAndDrop2 : MonoBehaviour, IPointerDownHandler, IBeginDragHandle
     public Canvas myCanvas;
     private CanvasGroup canvasGroup;
     public int id;
+    
     //store the starting position of each item in Vector2 variable
     public Vector2 initPos;
 
@@ -21,25 +22,25 @@ public class DragAndDrop2 : MonoBehaviour, IPointerDownHandler, IBeginDragHandle
     
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("BeginDrag");
+        //Debug.Log("BeginDrag");
        canvasGroup.blocksRaycasts = false;
     }
     
     // called on every frame moving object around
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+       // Debug.Log("OnDrag");
         // amount of movement done with mouse since last frame while holding the gameobject
         rectTrans.anchoredPosition += eventData.delta / myCanvas.scaleFactor;
     }
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("EndDrag");
+       // Debug.Log("EndDrag");
         canvasGroup.blocksRaycasts = true;
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("CLICK");
+        //Debug.Log("CLICK");
     }
 
     //reset position of game object to starting pisition
--- IlMioCorpo/Assets/Scripts/SlotScript.cs
+++ IlMioCorpo/Assets/Scripts/SlotScript.cs
@@ -6,10 +6,13 @@ using UnityEngine.EventSystems;
 public class SlotScript : MonoBehaviour, IDropHandler
 { 
     public int id;
+    public static int id_counter = 0;
 
     public void OnDrop(PointerEventData eventData)
     {
-        Debug.Log("Item Dropped");
+        
+        //Debug.Log("Item Dropped");
+        
         // properly adjust the position of the item inside of the slot.
         // open the slot script and check if eventData.pointerDrag is not null.
         // pointerDrag holds the data of the game object that is currently dragged
@@ -21,15 +24,25 @@ public class SlotScript : MonoBehaviour, IDropHandler
             // Check if item id matches slot id
             if(eventData.pointerDrag.GetComponent<DragAndDrop2>().id == id)
             {
+                id_counter++;
                  eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = this.GetComponent<RectTransform>().anchoredPosition;
+                 
                 //Debug.Log("CORRECT");
+                
             }
+            
                 else
                 {
                     eventData.pointerDrag.GetComponent<DragAndDrop2>().ResetPosition();
                     //Debug.Log("FALSE");
                 }
         }
+
+        if(id_counter ==  4)
+            {
+                Debug.Log("Corretto");
+            }
     }
+     
     
 }
