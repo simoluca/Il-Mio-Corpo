e13d9e2b4bc03797e31be824eceee5f112f02b3c
Simone <106522803+simoluca@users.noreply.github.com>
2022-11-16 09:52:19 +0100

    radiobutton toggle error

---
+++ IlMioCorpo/Assets/RadioButtonSystems.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class RadioButtonSystems : MonoBehaviour
+{
+    ToggleGroup toggleGroup;
+
+    
+    private void Start()
+    {
+        toggleGroup = GetComponent<ToggleGroup>();
+    }
+
+    public void Submit()
+    {
+        Toggle toggle = toggleGroup.ActiveToggles().FirstOrDefault();
+        Debug.Log(toggle.name + " _ " + toggle.GetComponentInChildren<Text>().text);
+    }
+}
