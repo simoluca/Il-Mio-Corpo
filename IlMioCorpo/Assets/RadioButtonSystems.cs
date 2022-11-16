530745dbba43c254571fe8746052a6a6ec2d9af4
Simone <106522803+simoluca@users.noreply.github.com>
2022-11-16 11:28:32 +0100

    1

--- IlMioCorpo/Assets/RadioButtonSystems.cs
+++ IlMioCorpo/Assets/RadioButtonSystems.cs
@@ -5,6 +5,7 @@ using System.Linq;
 public class RadioButtonSystems : MonoBehaviour
 {
     ToggleGroup toggleGroup;
+    public GameObject RetryText;
 
     
     private void Start()
@@ -16,11 +17,17 @@ public class RadioButtonSystems : MonoBehaviour
     {
         Toggle toggle = toggleGroup.ActiveToggles().FirstOrDefault();
        // Debug.Log(toggle.name + " _ " + toggle.GetComponentInChildren<Text>().text);
-       Debug.Log(toggle.name);
+      // Debug.Log(toggle.name);
 
        if(toggle.name == "Muscolo")
        {
             Debug.Log("ok");
+       }
+
+       if(toggle.name == "Grasso" || toggle.name == "Gomma")
+       {
+            RetryText.SetActive(true);
+            Debug.Log("riprova");
        }
     }
 
