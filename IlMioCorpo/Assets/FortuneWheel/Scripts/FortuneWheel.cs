5db714c18e829f2d923886e3de96dc1cf1183bfb
Simone <106522803+simoluca@users.noreply.github.com>
2022-11-30 12:59:57 +0100

    1

--- IlMioCorpo/Assets/FortuneWheel/Scripts/FortuneWheel.cs
+++ IlMioCorpo/Assets/FortuneWheel/Scripts/FortuneWheel.cs
@@ -40,7 +40,7 @@
         // list for active (non completed) quizes.
         // completed quizes will be removed from the list and not be considered by the wheel spin
         
-        List<int> QuizesList = new List<int>() {1,2,3,4,5,6,7,8,9,10,11,12};
+       List<int> QuizesList = new List<int>() {1,2,3,4,5,6,7,8,9,10,11,12};
 
         
 
