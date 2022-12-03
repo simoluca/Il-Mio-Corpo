9fd227e7cd28a64b5cc0f578b560b89189724753
Simone <106522803+simoluca@users.noreply.github.com>
2022-12-03 18:40:11 +0100

    1

--- IlMioCorpo/Assets/Scripts/EnabledOrDisabled.cs
+++ IlMioCorpo/Assets/Scripts/EnabledOrDisabled.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class EnabledOrDisabled : MonoBehaviour
 {
     public GameObject helpPopUp;
+    public GameObject helpPopUp2;
     public GameObject speechButton;
 
     public void Trigger(){
         if(helpPopUp.activeInHierarchy == false){
             helpPopUp.SetActive(true);
+            helpPopUp2.SetActive(true);
             speechButton.SetActive(true);
 
         }
         else{
             helpPopUp.SetActive(false);
+            helpPopUp2.SetActive(false);
             speechButton.SetActive(false);
         }
     }
