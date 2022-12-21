ee8938b335fb6b06a5045a1a1b72bae05f2a3518
Simone <106522803+simoluca@users.noreply.github.com>
2022-12-21 09:42:41 +0100

    1

--- IlMioCorpo/Assets/Scripts/ReadInputOssaPiedi.cs
+++ IlMioCorpo/Assets/Scripts/ReadInputOssaPiedi.cs
@@ -8,10 +8,11 @@ public class ReadInputOssaPiedi : MonoBehaviour
     // this will allow to store the text entry
     private string input;
     
-     public GameObject RetryText;
+    public GameObject RetryText;
     public GameObject CorrettoPopUp;
     public AudioSource CorrectAudio;
     public AudioSource WrongAudio;
+    public GameObject QuestionMark;
 
     public void ReadStringInput(string s)
     {
@@ -19,6 +20,7 @@ public class ReadInputOssaPiedi : MonoBehaviour
         if(s == "26")
         {
             CorrectAudio.Play();
+            QuestionMark.SetActive(false);
             CorrettoPopUp.SetActive(true);
         }
             else
